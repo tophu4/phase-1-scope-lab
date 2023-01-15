28df94134f3a506dd64869f1bd7485c0813f46d7
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-01-14 17:23:06 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+// customerName()
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+}
+
+// bestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// leastFavoriteCustomer
+const leastFavoriteCustomer = '';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bad customer';
+}
